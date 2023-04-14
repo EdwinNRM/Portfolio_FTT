e32bf4a911e643624e01f1989fe454d59740a825
Edwin Medina <edwinnrm@gmail.com>
2023-04-14 15:17:38 -0300

    finalização e alteração de estilo

--- app.py
+++ app.py
@@ -1,10 +1,59 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, flash
+from flask_mail import Mail, Message
+from config import email,senha,secret
 
 app = Flask(__name__)
+app.secret_key = secret
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.office365.com',
+    "MAIL_PORT": 587,
+    "MAIL_USE_TLS": True,
+    "MAIL_USE_SSL": False,
+    "MAIL_USERNAME": email,
+    "MAIL_PASSWORD": senha
+}
+
+app.config.update(mail_settings)
+
+mail = Mail(app)
+
+class Contato:
+    def __init__(self,nome,email,mensagem):
+        self.nome = nome,
+        self.email = email,
+        self.mensagem = mensagem
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/send', methods =['GET','POST'])
+def send():
+    if request.method == 'POST':
+        formContato = Contato(
+            request.form["nome"],
+            request.form["email"],
+            request.form["mensagem"]
+            )
+        
+        msg = Message(
+            subject = f'{formContato.nome} enviou uma mensagem',
+            sender = app.config.get("MAIL_USERNAME"),
+            recipients=["unievangelica.ftt@gmail.com"],
+            body = f'''
+
+            {formContato.nome} com o email {formContato.email}, te enviou uma mensagem:
+            
+            {formContato.mensagem}
+
+            '''
+        )
+
+        mail.send(msg)
+        flash('Mensagem enviada com sucesso!')
+    return redirect('/')
+
 
 if __name__ == 'main':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -18,3 +18,15 @@ navItem.forEach((item) => {
     }
   });
 });
+
+const btnEnviar = document.querySelector('#btn-enviar')
+const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
+
+btnEnviar.addEventListener("click", ()=>{
+  btnEnviarLoader.style.display = "block";
+  btnEnviar.style.display = "none"
+})
+
+setTimeout(() => {
+  document.querySelector('#alerta').style.display = 'none';
+}, 5000)
